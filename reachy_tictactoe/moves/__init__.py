2f03cab11676423943c5e0640bee2d8cd69a7e52
wnaiji <wnaiji@maison-intelligence-artificielle.com>
2026-02-10 08:46:54 +0100

    init rest_pos and base_pos

--- reachy_tictactoe/moves/__init__.py
+++ reachy_tictactoe/moves/__init__.py
@@ -19,22 +19,22 @@ moves = {
 
 
 rest_pos = {
-    'r_arm.r_shoulder_pitch': 4,
-    'r_arm.r_shoulder_roll': -16,
-    'r_arm.r_arm_yaw': -12,
-    'r_arm.r_elbow_pitch': -18,
-    'r_arm.r_forearm_yaw': -26,
-    'r_arm.r_wrist_pitch': -57,
-    'r_arm.r_wrist_roll': 13,
+    'r_arm.r_shoulder_pitch': 6,
+    'r_arm.r_shoulder_roll': -7,
+    'r_arm.r_arm_yaw': -25,
+    'r_arm.r_elbow_pitch': -84,
+    'r_arm.r_forearm_yaw': -10,
+    'r_arm.r_wrist_pitch': -9,
+    'r_arm.r_wrist_roll': 47,
 }
 
 base_pos = {
-    'r_arm.r_shoulder_pitch': 4,
-    'r_arm.r_shoulder_roll': -16,
-    'r_arm.r_arm_yaw': -12,
-    'r_arm.r_elbow_pitch': -18,
-    'r_arm.r_forearm_yaw': -26,
-    'r_arm.r_wrist_pitch': -57,
-    'r_arm.r_wrist_roll': 13,
+    'r_arm.r_shoulder_pitch': 6,
+    'r_arm.r_shoulder_roll': -7,
+    'r_arm.r_arm_yaw': -25,
+    'r_arm.r_elbow_pitch': -84,
+    'r_arm.r_forearm_yaw': -10,
+    'r_arm.r_wrist_pitch': -9,
+    'r_arm.r_wrist_roll': 47,
     'r_arm.r_gripper': GRIPPER_OPEN,
 }
